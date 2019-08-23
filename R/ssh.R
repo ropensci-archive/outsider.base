d46727a84a32269ced9385922dc265a3a722bb15
Dom Bennett <dominic.john.bennett@gmail.com>
2019-08-23 16:37:33 +0200

    dev-ing ssh

--- R/ssh.R
+++ R/ssh.R
@@ -1,19 +1,31 @@
-# TODO: allow servers
-
-# server_connect <- function(host, keyfile = NULL, passwd = getPass::getPass(),
-#                            verbose = FALSE) {
-#   # TODO: askpass or getPass?
-#   session <- ssh::ssh_connect(host = host, keyfile = keyfile, passwd = passwd,
-#                               verbose = verbose)
+# # TODO: allow servers
+# 
+# # if (!requireNamespace("ssh", quietly = TRUE)) {
+# #   msg <- paste0("Package ", char("ssh"), " required. Run ",
+# #                 elem("install.packages(\"ssh\")"), ' or similar.')
+# #   stop(msg, call. = FALSE)
+# # }
+# 
+# option_nm <- 'outsider-ssh-session'
+# 
+# server_connect <- function(session) {
 #   # set in options()
+#   options(option_nm = session)
 #   # create working dir
 #   invisible(TRUE)
 # }
 # 
 # server_disconnect <- function() {
-#   
+# 
 # }
 # 
 # is_server_connected <- function() {
-#   # does session exist in options?
+#   option_nm %in% names(options()) &&
+#     ssh::ssh_info(getOption(x = option_nm))[['connected']]
+# }
+# 
+# server_fetch <- function() {
+#   session <- getOption(x = option_nm)
+#   info <- ssh::ssh_info(session)
+#   paste0(info[['user']]
 # }
