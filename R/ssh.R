f664642cd475b33cd7e5ff3c3ad2893a656db59f
Dominic Bennett <dominic.john.bennett@gmail.com>
2019-08-23 11:11:37 +0200

    init sshing

---
+++ R/ssh.R
@@ -0,0 +1,19 @@
+# TODO: allow servers
+
+# server_connect <- function(host, keyfile = NULL, passwd = getPass::getPass(),
+#                            verbose = FALSE) {
+#   # TODO: askpass or getPass?
+#   session <- ssh::ssh_connect(host = host, keyfile = keyfile, passwd = passwd,
+#                               verbose = verbose)
+#   # set in options()
+#   # create working dir
+#   invisible(TRUE)
+# }
+# 
+# server_disconnect <- function() {
+#   
+# }
+# 
+# is_server_connected <- function() {
+#   # does session exist in options?
+# }
