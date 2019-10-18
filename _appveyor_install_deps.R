0e5b6643d50d1a6f640cad786bf7dc34d76e9551
Dom Bennett <dominic.john.bennett@gmail.com>
2019-10-18 15:06:29 +0200

    attempting own dep control

---
+++ _appveyor_install_deps.R
@@ -0,0 +1,27 @@
+# `travis-tool.sh install_deps` does not work for Windows Server 2019
+# appveyor calls this script instead
+
+# Vars ----
+# github repo
+repo <- 'AntonelliLab/outsider.base'
+
+# Functions ----
+read_deps <- function(repo) {
+  url <- paste0('https://raw.githubusercontent.com/', repo,
+                '/master/DESCRIPTION')
+  lns <- readLines(con = url)
+  strt <- which(grepl(pattern = 'Imports:', x = lns)) + 1
+  deps <- lns[strt:length(lns)]
+  deps <- deps[!grepl(pattern = 'Suggests:', x = deps)]
+  deps <- sub(pattern = '\\(.*\\)', replacement = '', x = deps)
+  deps <- gsub(pattern = '(\\s|,)', replacement = '', x = deps)
+  deps
+}
+
+# Install
+deps <- read_deps(repo = repo)
+for (dep in deps) {
+  cat('... Installing: [', dep, ']\n', sep='')
+  utils::install.packages(pkgs = dep,
+                          repos = 'https://ftp.acc.umu.se/mirror/CRAN/')
+}
