24ad64c20b385db8398af931a783f76ede5a29de
Dominic Bennett <dominic.john.bennett@gmail.com>
2020-02-16 14:48:32 +0100

    examples update, using donttest

--- examples/server.R
+++ examples/server.R
@@ -1,7 +1,9 @@
 library(outsider.base)
 
 # NOT RUN
-# session <- ssh::ssh_connect(host = '[INSERT HOST IP]')
-# server_connect(session = session)
-# # run outsider.base commands, when finished
-# server_disconnect()
+\dontrun{
+session <- ssh::ssh_connect(host = '[INSERT HOST IP]')
+server_connect(session = session)
+# run outsider.base commands, when finished
+server_disconnect()
+}
